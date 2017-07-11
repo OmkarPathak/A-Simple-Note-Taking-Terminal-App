8ba97be9efda7451ee80eee66c021e1a8635d437
Omkar Pathak <omkarpathak27@gmail.com>
2017-07-11 06:58:27 +0530

    A script to schedule emails at a specific time

--- ScheduleEmail.py
+++ ScheduleEmail.py
@@ -3,7 +3,7 @@ from crontab import CronTab
 # Get the crontab file for specific user. Type your username here
 my_cron = CronTab(user='omkarpathak')
 # Creating a new cron job
-job = my_cron.new(command='python /home/omkarpathak/Desktop/test.py')
+job = my_cron.new(command='python /home/omkarpathak/Documents/GITs/A-Simple-Note-Taking-Terminal-App/SendMail.py')
 # Run this job every minute
 job.minute.every(1)
 # Write this job in the cronjob file of this system
