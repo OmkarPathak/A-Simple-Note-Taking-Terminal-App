29981b3b036f76dbf7db5fec1d91d77ef74f3e7f
Omkar Pathak <omkarpathak27@gmail.com>
2017-07-13 07:16:52 +0530

    Sendmail file

--- SendMail.py
+++ SendMail.py
@@ -1,5 +1,6 @@
 # A helper function for sending emails
 import datetime
+import base64
 
 def sendMail(Message):
 	import smtplib
@@ -7,11 +8,13 @@ def sendMail(Message):
 	tadd = 'omkarpathak.comp@mmcoe.edu.in'         # receiver's email address
 	msg = Message                                  # Message to be sent!
 	username = 'omkarpathak.comp@mmcoe.edu.in'     # Your username(email ID)
-	password = '8149Omkar@'                        # Your password for above email ID
-	server = smtplib.SMTP('smtp.gmail.com',587)
+	with open('/home/omkarpathak/Documents/GITs/A-Simple-Note-Taking-Terminal-App/password.txt', 'rb') as password:
+		encryptedData = password.read()
+		password = base64.b64decode(encryptedData) # Your encrypted password for above email ID
+	server = smtplib.SMTP('smtp.gmail.com', 587)
 	server.ehlo()
 	server.starttls()
-	server.login(username,password)
+	server.login(username, password.decode('utf-8'))
 	server.sendmail(fadd,tadd,msg)
 	server.close()
 
@@ -27,7 +30,7 @@ with open('/home/omkarpathak/Documents/GITs/A-Simple-Note-Taking-Terminal-App/Sc
             time = datetime.time(int(currentTime[0]), int(currentTime[1])).strftime("%H:%M")
             res = date + ' ' + time
             now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
-            print(res, now)
+            # print(res, now)
             if res == now:
                 message = ' '.join(Message)
                 sendMail(message)
