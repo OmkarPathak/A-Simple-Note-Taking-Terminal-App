76e00b5510639d24a5d9e63316ca256f1ab13623
Omkar Pathak <omkarpathak27@gmail.com>
2017-07-16 11:58:33 +0530

    Added dynamic path logic

--- ScheduleEmail.py
+++ ScheduleEmail.py
@@ -1,11 +1,11 @@
 from crontab import CronTab
-import getpass
+import getpass, os
 import base64
 
 def setPassword():
     password = getpass.getpass('Enter your Email password for sending mails: ')
     encryptedData = base64.b64encode(password.encode('utf-8'))
-    with open('/home/omkarpathak/Documents/GITs/A-Simple-Note-Taking-Terminal-App/password.txt', 'wb') as password:
+    with open(os.path.dirname(os.path.abspath(__file__)) + '/password.txt', 'wb') as password:
         password.write(encryptedData)
 
 if __name__ == '__main__':
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     # Get the crontab file for specific user. Type your username here
     my_cron = CronTab(user='omkarpathak')
     # Creating a new cron job
-    job = my_cron.new(command='python /home/omkarpathak/Documents/GITs/A-Simple-Note-Taking-Terminal-App/SendMail.py')
+    job = my_cron.new(command='python ' + os.path.dirname(os.path.abspath(__file__)) + '/SendMail.py')
     # Run this job every minute
     job.minute.every(1)
     # Write this job in the cronjob file of this system
