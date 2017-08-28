07d0ef0aad712339e47df424064ba8c7dd3ea5b6
Omkar Pathak <omkarpathak27@gmail.com>
2017-08-28 07:38:49 +0530

    Added schema for postgresql

--- schema_postgresql.sql
+++ schema_postgresql.sql
@@ -1,1 +1,5 @@
-CREATE TABLE notes ("id" BIGSERIAL PRIMARY KEY, "created" TIMESTAMP DEFAULT now(),"updated" TIMESTAMP DEFAULT now(),"note" VARCHAR(255),"tag" VARCHAR(200));
+CREATE TABLE notes ("id" BIGSERIAL PRIMARY KEY,
+                    "created" TIMESTAMP DEFAULT now(),
+                    "updated" TIMESTAMP DEFAULT now(),
+                    "note" VARCHAR(255),
+                    "tag" VARCHAR(200));
